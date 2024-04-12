1e9d5f5441c1ee962266de12f30ce9f7b5fb0988
Wesley <124800998+wes-mendes@users.noreply.github.com>
2024-04-12 12:34:08 -0300

    Add files via upload

---
+++ ExerSite/ExerSite/Pessoa.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerSite
+{
+    public class Pessoa
+    {
+        private string nome;
+        private DateTime dataNasc;
+        private float altura;
+
+        public string Nome
+        {
+            get
+            {
+                return nome;
+            }
+            set
+            {
+                nome = value;
+            }
+        }
+
+        public DateTime DataNasc
+        {
+            get
+            {
+                return dataNasc;
+            }
+            set
+            {
+                dataNasc = value;
+            }
+        }
+
+        public float Altura
+        {
+            get
+            {
+                return altura;
+            }
+            set
+            {
+                altura = value;
+            }
+        }
+
+        public void ImprimirDados()
+        {
+            Console.WriteLine("Nome: " + nome);
+            Console.WriteLine("Data de Nascimento: " + dataNasc);
+            Console.WriteLine("Altura: " + altura);
+        }
+
+        public int CalcularIdade()
+        {
+            return DateTime.Today.Year - dataNasc.Year;
+        }
+
+    }
+}
---
+++ ExerSite/ExerSite/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ExerSite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p = new Pessoa();
+            p.Nome = "Joel";
+            p.DataNasc = new DateTime(1993, 6, 28);
+            p.Altura = 1.76f;
+
+            p.ImprimirDados();
+
+            int idade = p.CalcularIdade();
+
+            Console.WriteLine("Idade: " + idade);
+
+            Console.ReadKey();
+        }
+    }
+}
